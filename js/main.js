1a997e8f2a7c081a35925616baecfd67760b84b4
Mohamed Zaitoon <mohamedzaitoon72@gmail.com>
2019-02-25 14:57:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Mohamed Zaitoon.", "Android Developer.", "Web Developer."],
       typeSpeed: 100,
       loop: true,
     });
